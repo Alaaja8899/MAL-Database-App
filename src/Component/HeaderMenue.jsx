3deb59f81bbb0b0e1819cd74c46fcd6d6cb6a5cf
Abdirizak abdullahi hussein <102373033+Alaaja8899@users.noreply.github.com>
2024-07-30 12:57:40 +0300

    Add files via upload

--- src/Component/HeaderMenue.jsx
+++ src/Component/HeaderMenue.jsx
@@ -5,10 +5,17 @@ function HeaderMenue() {
   return (
     <header className='h-[83px] bg-slate-50 bg-mainColor text-[#fff] fixed left-0 right-0'>
 
-        <div className="container mx-auto flex items-center justify-between px-4 py-2 h-full md:flex-row flex-col">
+        <div className="container mx-auto flex items-center justify-between px-4 py-2 h-full md:flex-row ">
         <h2 className='flex items-center font-bold text-2xl h-full gap-2'>
             <img src={logo}  className="max-h-full" />
         </h2>
+
+
+          <a href="https://github.com/Alaaja8899/MAL-Database-App" target='_blank'>
+            <img
+            className='w-[40px] h-[40px] rounded-full'
+            src="https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png"/>
+          </a>
 
 
         </div>
